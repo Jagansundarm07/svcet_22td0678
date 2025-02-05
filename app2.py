1b2493a40b92dcc1535b63a05088f2b8a32fa9c4
Jagansundarm07 <jagansundar22td0678@svcet.ac.in>
2025-02-05 10:16:23 +0530

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,28 +1,15 @@
-from flask import Flask,request
+from flask import Flask, render_template, request
+from forms import NameForm
+
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return "welcome to flask!"
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a real secret key
 
-@app.route('/hellow/<name>')
-def hello(name):
-    return f"hello {name}"
-
-@app.route('/data',methods=['GET'])
-def get_data():
-    data=request.json
-    return {"data":data,"message":"send a POST request with JSON data"}
-@app.route('/data',methods=['POST'])
-def handle_data():
-    data=request.json
-    return {"message":"data received","data":data}
-
-@app.route('/update/<int:item_id>',methods=['PUT'])
-def update_item(item_id):
-    return {"message":f"Item{item_id} updated"}
-@app.route('/delete/<int:item_id>',methods=['DELETE'])
-def delete_item(item_id):
-    return {"message":f"Item{item_id} deleted"}
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = NameForm()
+    if form.validate_on_submit():
+        return f"Hello, {form.name.data}!"
+    return render_template('form.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
